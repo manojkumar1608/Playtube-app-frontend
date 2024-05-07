293cd4f72cce0105f8e2ff5ae82447fa08fd4766
manoj <pitlamanoj1@gmail.com>
2024-05-07 16:54:44 +0530

    proxy updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api' :'https://playtube-app-frontend.vercel.app'
+      '/api' :'https://playtube-app-backend.onrender.com'
     },
   },
   plugins: [react()]
