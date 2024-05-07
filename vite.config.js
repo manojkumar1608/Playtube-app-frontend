0a0fea6ad1ba68c288c9c03f060ea7e563931905
manoj <pitlamanoj1@gmail.com>
2024-05-07 23:15:11 +0530

    proxy changed

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      // '/api' :'https://playtube-app-backend.onrender.com'
-    },
+  proxy: {
+    // Define your proxy configuration here for development.
+    // This is not used in production. Production proxying should
+    // typically be handled by the server hosting your application.
+    '/api': {
+      target: 'https://playtube-app-backend.onrender.com',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    }
   },
   plugins: [react()]
   
