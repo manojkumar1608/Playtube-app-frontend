e155707519c3c6f077ed7c00a367fc0777fb028b
manoj <pitlamanoj1@gmail.com>
2024-05-08 17:52:39 +0530

    axios updated for logout url

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
-import {logout as authlogout} from '../../store/authSlice'
+import { logout as authlogout } from '../../store/authSlice'
 import axios from 'axios'
-import  { useDispatch }  from 'react-redux'
-import { useNavigate }  from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import Button from '../utilities/Button'
-function  LogoutBtn() {
+function LogoutBtn() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-    const logoutHandler = async()=>{
-      const res = await axios.post('https://playtube-app-backend.onrender.com/api/v1/users/logout')
-      if(res)
-        dispatch(authlogout())
-      navigate('/')
-    }
+  const logoutHandler = async () => {
+    const res = await axios({
+      method: 'POST',
+      url: 'https://playtube-app-backend.onrender.com/api/v1/users/logout',
+      withCredentials:true,
+
+    })
+    if (res)
+      dispatch(authlogout())
+    navigate('/')
+  }
 
   return (
     <div className=''>
-               
+
       <Button onClick={logoutHandler}
-         className='w-24 text-gray-200 border bg-gradient-to-t from-red-800 to-gray-950 rounded-xl font-bold'>Logout
+        className='w-24 text-gray-200 border bg-gradient-to-t from-red-800 to-gray-950 rounded-xl font-bold'>Logout
 
-        </Button>
+      </Button>
 
-  </div>
+    </div>
   )
 }
 
